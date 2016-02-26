5da13343d6ad84ad3388dde1f2b7ce1e0003029b
Tony Bowden <tony@tmtm.com>
2016-02-26 21:57:12 +0000

    Modernise scraper

--- scraper.rb
+++ scraper.rb
@@ -1,17 +1,9 @@
 #!/bin/env ruby
 # encoding: utf-8
 
-require 'scraperwiki'
 require 'wikidata/fetcher'
-require 'rest-client'
 
-ru = WikiData::Category.new('Категория:Депутаты Жогорку Кенеша', 'ru').wikidata_ids
-en = WikiData::Category.new('Category:Members of the Supreme Council (Kyrgyzstan)', 'en').wikidata_ids
+ru = WikiData::Category.new('Категория:Депутаты Жогорку Кенеша', 'ru').member_titles
+en = WikiData::Category.new('Category:Members of the Supreme Council (Kyrgyzstan)', 'en').member_titles
 
-(ru + en).flatten.uniq.each do |id|
-  data = WikiData::Fetcher.new(id: id).data('ky', 'ru') or next
-  ScraperWiki.save_sqlite([:id], data)
-end
-
-warn RestClient.post ENV['MORPH_REBUILDER_URL'], {} if ENV['MORPH_REBUILDER_URL']
-
+EveryPolitician::Wikidata.scrape_wikidata(names: { en: en, ru: ru})
