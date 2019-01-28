cf47313c9292efa0ab95bd838c605a597fab62b1
Tony Bowden <tony@tmtm.com>
2019-01-28 19:39:12 +0000

    Also find via Wikidata P39s

--- scraper.rb
+++ scraper.rb
@@ -3,7 +3,16 @@
 
 require 'wikidata/fetcher'
 
-ru = WikiData::Category.new('Категория:Депутаты Жогорку Кенеша', 'ru').member_titles
-en = WikiData::Category.new('Category:Members of the Supreme Council (Kyrgyzstan)', 'en').member_titles
+ru = WikiData::Category.new('Категория:Депутаты Жогорку Кенеша', 'ru').wikidata_ids
+en = WikiData::Category.new('Category:Members of the Supreme Council (Kyrgyzstan)', 'en').wikidata_ids
 
-EveryPolitician::Wikidata.scrape_wikidata(names: { en: en, ru: ru})
+query = <<SPARQL
+  SELECT DISTINCT ?item WHERE {
+    ?item p:P39 [ ps:P39 wd:Q21091853 ; pq:P2937 ?term ] .
+    ?term p:P31/pq:P1545 ?ordinal .
+    FILTER (xsd:integer(?ordinal) >= 5)
+  }
+SPARQL
+p39s = EveryPolitician::Wikidata.sparql(query)
+
+EveryPolitician::Wikidata.scrape_wikidata(ids: en | ru | p39s)
